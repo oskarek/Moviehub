142cee7a61370534bc92a8c1115a3a1aaaae1b92
Oskar Ek <oskarek24@gmail.com>
2019-11-20 22:27:54 +0100

    Styling updates to SearchBar

--- Search/SearchBar.swift
+++ Search/SearchBar.swift
@@ -13,16 +13,23 @@ public struct SearchBar : View {
     HStack {
       Image(systemName: "magnifyingglass").foregroundColor(.secondary)
       TextField(self.title, text: $searchText)
+        .disableAutocorrection(true)
       if !searchText.isEmpty {
-        Image(systemName: "xmark.circle.fill").foregroundColor(.secondary)
+        Button(action: { self.searchText = "" }) {
+          Image(systemName: "xmark.circle.fill").foregroundColor(.secondary)
+        }
       }
-    }.padding(.horizontal)
+    }
+    .padding(.init(top: 8, leading: 8, bottom: 8, trailing: 8))
+    .background(Color.gray.opacity(0.2))
+    .cornerRadius(10)
+    .padding(.horizontal)
   }
 }
 
 struct SearchBar_Previews: PreviewProvider {
   static var previews: some View {
-    let layout = PreviewLayout.fixed(width: 400, height: 50)
+    let layout = PreviewLayout.fixed(width: 400, height: 65)
     return SearchBar(title: "Search", searchText: .constant(""))
       .previewLayout(layout)
   }
